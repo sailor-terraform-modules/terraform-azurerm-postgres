1001ec9dbb3536316d7818048cd3649f938bdb87
RaviShankar <ravishankar@Ravi-MacBook-Air.local>
2023-06-05 16:43:16 +0530

    inital commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_postgresql_server" "postgresql_server" {
+  name                              = var.name
+  location                          = var.location
+  resource_group_name               = var.resource_group_name
+  administrator_login               = var.administrator_login
+  administrator_login_password      = var.administrator_login_password
+  sku_name                          = var.sku_name
+  version                           = var.postgresql_version
+  storage_mb                        = var.storage_mb
+  backup_retention_days             = var.backup_retention_days
+  geo_redundant_backup_enabled      = var.geo_redundant_backup_enabled
+  public_network_access_enabled     = var.public_network_access_enabled
+  ssl_enforcement_enabled           = var.ssl_enforcement_enabled
+  ssl_minimal_tls_version_enforced  = var.ssl_minimal_tls_version_enforced
+}
---
+++ variable.tf
@@ -0,0 +1,70 @@
+variable "resource_group_name" {
+  type        = string
+  description = "name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "location of the resource group"
+}
+
+variable "name" {
+  type        = string
+  description = "Name of the postgresql_server"
+}
+
+variable "administrator_login" {
+  type        = string
+  description = "username of the azurerm_postgresql_server"
+}
+
+variable "administrator_login_password" {
+  type        = string
+  description = "password of the azurerm_postgresql_server"
+}
+
+variable "sku_name" {
+  type        = string
+  description = "The name of the SKU, follows the tier + family + cores pattern (e.g. B_Gen4_1, GP_Gen5_8)"
+}
+
+variable "storage_mb" {
+  type        = string
+  description = "Max storage allowed for a server.values are between 5120 MB(5GB) and 1048576 MB(1TB) for the Basic SKU and between 5120 MB(5GB) and 16777216 MB(16TB) for General Purpose/Memory Optimized SKUs. For more information see the product documentation."
+}
+
+variable "postgresql_version" {
+  type        = string
+  description = "The version of the postgressql"
+}
+
+
+variable "backup_retention_days" {
+  type        = number
+  description = "name of the resource group"
+  default = 7
+}
+
+variable "geo_redundant_backup_enabled" {
+  type        = bool
+  description = "location of the resource group"
+  default = false
+}
+
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "Name of the postgresql_server"
+  default = true
+}
+
+variable "ssl_enforcement_enabled" {
+  type        = bool
+  description = "Name of the postgresql_server"
+  default = false
+}
+
+variable "ssl_minimal_tls_version_enforced" {
+  type        = string
+  description = "password of the azurerm_postgresql_server"
+  default = "TLSEnforcementDisabled"
+}
