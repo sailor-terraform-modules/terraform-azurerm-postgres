256272f15732418741c849499c95a7308f5967b5
anaghsn <130640443+SnAnagh@users.noreply.github.com>
2023-09-06 17:08:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_key_vault" "key_vault" {
   logging {
     enabled = true
   }
-  
+
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
